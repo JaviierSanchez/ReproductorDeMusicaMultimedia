95f3bdda61d02cf9982abcc7082a23299c4bdd6a
Javiersc647 <javiersc16@educarex.es>
2024-02-05 12:23:53 +0100

    v

--- app/src/main/java/com/example/reproductordemusica/MainActivity.java
+++ app/src/main/java/com/example/reproductordemusica/MainActivity.java
@@ -3,43 +3,141 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;
     int pause;
+    SeekBar selector;
+    Runnable runnable;
+    Handler handler;
+    CheckBox bucle;
+
+    TextView duracion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bucle = findViewById(R.id.checkBoxBucle);
+        duracion = findViewById(R.id.tvTiempo);
+        selector = findViewById(R.id.seekBar);
+        handler = new Handler();
+        selector.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    if (mp != null) {
+                        mp.seekTo(progress);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
-    public void play(View view){
+    private String obtenerDuracionFormateada(int millis) {
+        int segundos = millis / 1000;
+        int minutos = segundos / 60;
+        segundos = segundos % 60;
+        return String.format("%02d:%02d", minutos, segundos);
+    }
+
+    public void bucleCancion(View view){
+
+        if (bucle.isChecked()) {
+            if (mp != null) {
+                mp.setLooping(true);
+            }
+        } else {
+            if (mp != null) {
+                mp.setLooping(false);
+            }
+        }
+    }
+
+
+
+    public void play(View view) {
         if (mp == null) {
-            mp = MediaPlayer.create(this, R.raw.musica3);
-            mp.start();
-            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
-        }else if (!mp.isPlaying()){
+            mp = MediaPlayer.create(this, R.raw.musica2);
+            if (mp != null) {
+                mp.start();
+                Toast.makeText(this, "Reproduciendo", Toast.LENGTH_SHORT).show();
+                selector.setMax(mp.getDuration());
+                actualizarSeekBar();
+            } else {
+                Toast.makeText(this, "Error al crear el reproductor", Toast.LENGTH_SHORT).show();
+            }
+        } else if (!mp.isPlaying()) {
             mp.seekTo(pause);
             mp.start();
-            Toast.makeText(this, "Renaudando", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Reanudando", Toast.LENGTH_SHORT).show();
         }
-
     }
-    public void pause(View view){
 
-        if (mp != null){
+
+
+
+    public void pause(View view) {
+        if (mp != null && mp.isPlaying()) {
             mp.pause();
             pause = mp.getCurrentPosition();
-            Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
         }
     }
-    public void stop(View view){
-        mp.stop();
-        mp = null;
-        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
 
+    public void stop(View view) {
+        if (mp != null && mp.isPlaying()) {
+            mp.stop();
+            mp.release();
+            mp = null;
+            selector.setProgress(0);
+            bucle.setChecked(false); // Desactivar el bucle al detener la reproducción
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void actualizarSeekBar() {
+        if (mp != null) {
+            int currPos = mp.getCurrentPosition();
+            selector.setProgress(currPos);
+
+            // Obtén la duración total de la canción en milisegundos
+            int duracionTotal = mp.getDuration();
+
+            // Actualiza el TextView con el formato "Duración: mm:ss"
+            String duracionFormateada = obtenerDuracionFormateada(currPos) + "/" + obtenerDuracionFormateada(duracionTotal);
+            duracion.setText("Duración: " + duracionFormateada);
+
+            runnable = () -> actualizarSeekBar();
+            handler.postDelayed(runnable, 1000);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mp != null) {
+            mp.release();
+            mp = null;
+        }
+        handler.removeCallbacksAndMessages(null);
     }
 }
